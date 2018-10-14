878c0ffbf2c21212532e1b5ece47c6c2cb4ed4e6
jasa1704 <jasosapanta@gmail.com>
2018-10-14 13:43:45 -0500

    Cambios interfaz home

--- frontend.js
+++ frontend.js
@@ -6,8 +6,8 @@ function estilosV(estilosv)
     var series = "";
 
     estilosv.forEach(item => {
-            series += "<div class='card ml-5 mb-5 mt-3'>" + "<div class='card-header text-center'>" + item.estilo_saludable + "</div>" + 
-            "<div class='card-body'>" + "<img src='" + item.imagen + "' alt='imagen' width='270' height='200'>" + "</div>" + "</div>"
+            series += "<div class='card ml-5 mb-5 mt-3'>" + "<div class='card-header text-center'>" + item.estilo_saludable + "</div>" 
+            + "<a href='" + item.enlace + "'>" + "<div class='card-body margen'>" + "<img src='" + item.imagen + "' alt='imagen' width='100' height='100'>" + "</div>" + "</a>" + "</div>"
             cont.innerHTML = series;
     });
     
